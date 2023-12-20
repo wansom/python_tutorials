872db6cea1a4c87cfee5093a8fcc6abb8299059c
wansom <ochiengwarren10@gmail.com>
2023-12-20 04:03:00 +0300

    data_types ,strings, numbers,lists

---
+++ data_types.py
@@ -0,0 +1,24 @@
+
+# Strings
+first_name='warren'
+last_name="ochieng"
+my_fav_color=" maroon"
+full_name=  first_name + '' + last_name
+message=f"my name is {first_name} {last_name} and my favorite color is {my_fav_color}"
+
+print(my_fav_color)
+print(my_fav_color.count('mar'))
+print(first_name.find('w'))
+print(full_name)
+print(message)
+# print(dir(str))
+
+# numbers
+age =26
+rent = 30000
+rent_in_words=str(rent)
+print(rent_in_words)
+
+# Lists 
+courses=['history','math','history','math']
+print(len(courses))
